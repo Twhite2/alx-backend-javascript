c979146ee54ddad91f320ac0056f63bd3f210b45
Twhite2 <emmanuelmieye@gmail.com>
2023-01-24 00:20:17 +0000

    advanced tasks

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+	  const inter = [];
+	   eslint-disable-next-line
+	   for (const [k, v] of Object.entries(report)) {
+	       if (v instanceof Array) inter.push(...v);
+	         }
+           return inter;
+	           }
