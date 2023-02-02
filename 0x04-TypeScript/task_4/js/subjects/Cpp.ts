63ce01c71d6173dd48e161e8c3bf769a0f95e689
Twhite2 <emmanuelmieye@gmail.com>
2023-02-02 01:51:29 +0100

    task5

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,37 +1,18 @@
 namespace Subjects {
-    export interface Teacher {
-      firstName: string;
-      lastName: string;
-      experienceTeachingC?: number;
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subjects.Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
     }
-    
-    export class Subject {
-      private _teacher: Teacher;
-      
-      set teacher(teacher: Teacher) {
-        this._teacher = teacher;
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC <= 0) {
+        return 'No available teacher';
       }
-      
-      get teacher(): Teacher {
-        return this._teacher;
-      }
-    }
-    
-    export class Cpp extends Subject {
-      getRequirements(): string {
-        return "Here is the list of requirements for Cpp: " + 
-               "Experience in C++ programming, Knowledge of Object-Oriented Programming concepts";
-      }
-      
-      getAvailableTeacher(): string {
-        if (!this.teacher) return "No available teacher";
-        if (this.teacher.experienceTeachingC === undefined || 
-            this.teacher.experienceTeachingC === null || 
-            this.teacher.experienceTeachingC === 0) {
-          return "No available teacher";
-        }
-        return "Available Teacher: " + this.teacher.firstName;
-      }
+      return `Available Teacher: ${this.teacher.firstName}`;
     }
   }
-  
+}
